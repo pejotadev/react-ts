136953bb55830ff1a962e616a1730871bfc19748
Pedro José NPS <pedrojosepaiva@outlook.com>
2024-01-04 16:50:02 -0300

    feat: initialize repo with react + ts + vite

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      HEllO WORLD
+    </div>
+  );
+}
+
+export default App;
+
+
+/* <UiContextProvider>
+      <UserContextProvider>
+        <Header />
+        <Content />
+        <Form />
+      </UserContextProvider>
+    </UiContextProvider> */
